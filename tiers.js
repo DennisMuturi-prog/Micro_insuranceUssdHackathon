8a17becee627870babcc8e46437e382cef686b07
Dennis Muturi <denismutur@gmail.com>
2024-02-24 18:48:47 +0300

    Add tiers.js file with tier descriptions

---
+++ tiers.js
@@ -0,0 +1,27 @@
+const tiers=
+['tiers',
+`Tier 1 - Preventive Care:
+
+Services include annual check-ups, vaccinations, and screenings.
+Cost-sharing: Low or no out-of-pocket costs for policyholders.`,
+`Tier 2 - Basic Services:
+
+Outpatient care, doctor visits, and generic prescription drugs.
+Cost-sharing: Moderate copayments or coinsurance.`,
+`Tier 3 - Specialized Services:
+
+Specialist consultations, diagnostic imaging, and brand-name prescription drugs.
+Cost-sharing: Higher copayments or coinsurance.`,
+`Tier 4 - Hospitalization and Surgery:
+
+Inpatient hospital stays, surgeries, and emergency room visits.
+Cost-sharing: Higher coinsurance, possibly with an annual deductible.`,
+`Tier 5 - Advanced and Specialty Care:
+
+Mental health services, maternity care, and rehabilitation services.
+Cost-sharing: Higher coinsurance or copayments.`,
+`Tier 6 - Complex Procedures:
+
+Coverage for complex and specialized treatments, including organ transplants or experimental procedures.
+Cost-sharing: Highest coinsurance, possibly subject to pre-authorization.`];
+module.exports={tiers}
